f7e7828cc4492b8c4ba5033a2b98b0d90d01e87b
Hester Lai <h.lai52@gmail.com>
2022-12-03 19:00:34 -0800

    Client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import TodoList from "./components/todo/TodoList";
+function App() {
+  return (
+    <div className="App">
+      <h1> Aloha! Welcome, Hester</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
