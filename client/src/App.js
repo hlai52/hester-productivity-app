a4d5239de596df2ead8700ed1afd5e7c599f956f
Hester Lai <h.lai52@gmail.com>
2022-12-05 14:36:54 -0800

    Added a clock

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import TodoList from "./components/todo/TodoList";
+import Clock from "./components/clock/Clock";
 function App() {
   return (
-    <div className="App">
-      <h1> Aloha! Welcome, Hester</h1>
-      <TodoList />
-    </div>
+    <>
+      <div>
+        <Clock />
+      </div>
+      <div className="App">
+        <h1> Aloha! Welcome, Hester</h1>
+        <TodoList />
+      </div>
+    </>
   );
 }
 
