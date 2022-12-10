295e5840c59c859b8f2603e220c9d6f3b159604f
Hester Lai <h.lai52@gmail.com>
2022-12-10 03:29:14 -0800

    updated and fixed timer with minutes working

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -1,4 +1,4 @@
-import { useState, React } from "react";
+import { useState, React, useCallback } from "react";
 //
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import "./timer.css";
@@ -24,25 +24,56 @@ const renderTime = ({ remainingTime }) => {
 };
 
 function Timer() {
-  const [key, setKey] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [timerMinutes, setTimerMinutes] = useState(30);
+
+  const incrementTimer = useCallback(() => {
+    setTimerMinutes(timerMinutes + 5);
+  }, [timerMinutes]);
+
+  const decrementTimer = useCallback(() => {
+    if (timerMinutes > 5) {
+      setTimerMinutes(timerMinutes - 5);
+    }
+  }, [timerMinutes]);
+
+  const startTimer = useCallback(() => {
+    setIsRunning(true);
+  }, []);
+
+  const pauseTimer = useCallback(() => {
+    setIsRunning(false);
+  }, []);
+
   return (
     <div className="App">
       <h1>
         Timer :
         <br />
-        30 mins
+        {`${timerMinutes} mins`}
       </h1>
+      <button className="info" onClick={incrementTimer}>
+        Add 5mns
+      </button>
+      <button className="info" onClick={decrementTimer}>
+        Remove 5mns
+      </button>
       <div className="timer-wrapper">
         <CountdownCircleTimer
-          isPlaying
-          duration={1800}
+          isPlaying={isRunning}
+          duration={60 * timerMinutes}
           colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
           onComplete={() => [true, 1000]}
         >
           {renderTime}
         </CountdownCircleTimer>
       </div>
-      <button className="info"> Click here to start!</button>
+      <button className="info" onClick={startTimer}>
+        Click here to start!
+      </button>
+      <button className="info" onClick={pauseTimer}>
+        Click here to pause!
+      </button>
     </div>
   );
 }
